52c9e0f6c9f71cc38de331e6f530a324ea0180ae
rhfovk <rhfovk@gmail.com>
2021-07-08 20:34:08 +0900

    Delete 알고리즘/for문 directory

--- 알고리즘/for문/N.js
+++
@@ -1,5 +0,0 @@
-let n = 9;
-
-for(i=0; i<n; i++){
-    console.log(n - i);
-}
--- 알고리즘/for문/gugu.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-let num = 9;
-
-for(let i=1; i<=9; i++){
-    console.log(`${num} * ${i} = ${num * i}`)
-}
